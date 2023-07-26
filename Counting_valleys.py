8076b4f03142df5ff27f2f085b93bb89226d7827
babygrim <bstetskiv@gmail.com>
2023-07-26 19:03:42 +0300

    changes Counting_valleys

--- Counting_valleys.py
+++ Counting_valleys.py
@@ -1,14 +1,14 @@
 def countingValleys(steps, path):
     count = 0
     sea_level = 0
-    i = 0
-    while i <= steps-1:
-        sea_level += 1 if path[i] == "U" else -1
-        if sea_level == 0 and path[i] == "U":
-            count += 1
-        i += 1
-            
-    return count
+    for i in range(steps):
+        if path[i] == "D":
+            sea_level -= 1
+        else:
+            sea_level += 1
+            if sea_level == 0:
+                count += 1
     
+    return count
 
 print(countingValleys(8, "UDDDUDUU"))
